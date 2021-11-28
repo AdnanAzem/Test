3bb488d8814e1e62af07accc6dfa8005c3e073ec
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-11-28 17:04:53 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -2,11 +2,12 @@
 #define my_mat
 #include <stdio.h>
 int findPath(int x, int y);
-void isThereConnection();
+
 void buildGraph();
 //void findPath(int x, int y);
 void min_temp();
 void Dijkstra(int x);
 void shortPath();
+void isThereConnection();
 
 #endif
