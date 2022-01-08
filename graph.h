e3d82fc97a8e2b1789b05a7c67879a612d724758
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2022-01-08 13:58:52 +0200

    Update graph.h

--- graph.h
+++ graph.h
@@ -24,7 +24,15 @@ void setWeight(pnode *head, int key, int weight);
 //void delete_edge(pnode *head,int id);
 pnode getNode(pnode *head, int id);
 int shortsPath_cmd2(pnode head,int src ,int dest);
-void shortsPath_cmd(pnode head);
+//void shortsPath_cmd(pnode head);
+
+
+
+
+
+
+
+
 
 void build_graph_cmd(pnode *head);
 void insert_node_cmd(pnode *head);
@@ -32,7 +40,10 @@ void delete_node_cmd(pnode *head);
 void printGraph_cmd(pnode head); //for self debug
 void deleteGraph_cmd(pnode* head);
 //int shortsPath_cmd(pnode head, int src, int dest);
-int TSP_cmd(pnode head);
+//int TSP_cmd(pnode head);
+int shortest_path(pnode* head, int src, int dest);
+//int TSP(pnode *head);
+void tsp(pnode *head);
 //void TSP_cmd(pnode head);
 //pnode getNode(pnode *head, int id);
 
